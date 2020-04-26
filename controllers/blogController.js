bf39bc3bc1670eff41d9e1c6c01bb4952ddc9ea4
Manik Kapoor <mkapoor765@gmail.com>
2020-04-26 19:09:34 +0530

    first-commit

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,8 +1,9 @@
 const express=require('express');
 const mongoose=require('mongoose');
+const shortid=require('shortid')
 
 const BlogModel=mongoose.model('Blog')
-mongoose.connect('mongodb://localhost');
+//  mongoose.connect('mongodb://localhost');
 
 let helloWorldFunction=(req,res)=>res.send('Hello World')
 let printExample=(req,res)=>res.send("print example");
@@ -37,8 +38,148 @@ let createBlog=(req,res)=>{
         author:req.body.fullName,
         crea
     })
+    let tags=(req.body.tags!=undefined&&req.body.tags!=null&&req.body.tags!='')?req.body.tags.split(','):[] 
+    newBlog.tags=tags
+    newBlog.save((err,result)=>{
+        if(err){
+            console.log(err)
+            res.send(err)
+        }
+        else{
+            res.send(result)
+        }
+    })
+}
+/**
+ * function to read single blog.
+ */
+let viewByBlogId = (req, res) => {
+
+    BlogModel.findOne({ 'blogId': req.params.blogId }, (err, result) => {
+
+        if (err) {
+            console.log(err)
+            res.send(err)
+        } else if (result == undefined || result == null || result == '') {
+            console.log('No Blog Found')
+            res.send("No Blog Found")
+        } else {
+            res.send(result)
+
+        }
+    })
+}
+/**
+ * function to read blogs by category.
+ */
+let viewByCategory = (req, res) => {
+
+    BlogModel.find({ 'category': req.params.category }, (err, result) => {
+
+        if (err) {
+            console.log(err)
+            res.send(err)
+        } else if (result == undefined || result == null || result == '') {
+            console.log('No Blog Found')
+            res.send("No Blog Found")
+        } else {
+            res.send(result)
+
+        }
+    })
 }
 
+/**
+ * function to read blogs by author.
+ */
+let viewByAuthor = (req, res) => {
+
+    BlogModel.find({ 'author': req.params.author }, (err, result) => {
+
+        if (err) {
+            console.log(err)
+            res.send(err)
+        } else if (result == undefined || result == null || result == '') {
+            console.log('No Blog Found')
+            res.send("No Blog Found")
+        } else {
+            res.send(result)
+
+        }
+    })
+}
+/**
+ * function to edit blog by admin.
+ */
+let editBlog = (req, res) => {
+
+    let options = req.body;
+    console.log(options);
+    BlogModel.update({ 'blogId': req.params.blogId }, options, { multi: true }).exec((err, result) => {
+
+        if (err) {
+            console.log(err)
+            res.send(err)
+        } else if (result == undefined || result == null || result == '') {
+            console.log('No Blog Found')
+            res.send("No Blog Found")
+        } else {
+            res.send(result)
+
+        }
+    })
+}
+
+
+
+/**
+ * function to delete the assignment collection.
+ */
+let deleteBlog = (req, res) => {
+    BlogModel.remove({ 'blogId': req.params.blogId }, (err, result) => {
+        if (err) {
+            console.log(err)
+            res.send(err)
+        } else if (result == undefined || result == null || result == '') {
+            console.log('No Blog Found')
+            res.send("No Blog Found")
+        } else {
+            res.send(result)
+
+        }
+    })
+}
+/**
+ * function to increase views of a blog.
+ */
+let increaseBlogView = (req, res) => {
+
+    BlogModel.findOne({ 'blogId': req.params.blogId }, (err, result) => {
+
+        if (err) {
+            console.log(err)
+            res.send(err)
+        } else if (result == undefined || result == null || result == '') {
+            console.log('No Blog Found')
+            res.send("No Blog Found")
+        } else {
+            
+            result.views += 1;
+            result.save(function (err, result) {
+                if (err) {
+                    console.log(err)
+                    res.send(err)
+                }
+                else {
+                    console.log("Blog updated successfully")
+                    res.send(result)
+
+                }
+            });// end result
+
+        }
+    })
+}
 module.exports={
     helloWorldFunction:helloWorldFunction,
     printExample:printExample,
